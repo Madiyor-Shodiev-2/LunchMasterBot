03e025745e07ed8c22f743bb2dbdf42e18164073
Rayid32 <madiyorshodiev333@gmail.com>
2025-06-07 10:55:12 +0500

    MAJOR: Added models and migrations

--- app/Models/LunchSchedule.php
+++ app/Models/LunchSchedule.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class LunchSchedule extends Model
 {
-    //
+    protected $table = "lunch_schedules";
+    protected $primaryKey = 'schedule_id';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'name',
+        'hour',
+        'minute',
+        'max_per_round',
+        'active'
+    ];
+
+    public function sessions() {
+        return $this->hasMany(
+            related: LunchSession::class,
+            foreignKey: 'schedule_id',
+            localKey: 'schedule_id'
+        );
+    }
 }
--- app/Models/LunchSession.php
+++ app/Models/LunchSession.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class LunchSession extends Model
 {
-    //
+    protected $table = "lunch_sessions";
+    protected $primaryKey = 'session_id';
+    public $incrementing = true;
+    protected $fillable = [
+        'schedule_id',
+        'date',
+        'status'
+    ];
+
+    public function schedule(){
+        return $this->belongsTo(
+            related: LunchSchedule::class,
+            foreignKey: 'schedule_id',
+            ownerKey: 'schedule_id'
+        );
+    }
+
+    public function operators(){
+        return $this->belongsToMany(
+            related: Operator::class,
+            table: 'queue_entries', 
+            foreignPivotKey: 'session_id', 
+            relatedPivotKey: 'operator_id'
+        );
+    }
 }
